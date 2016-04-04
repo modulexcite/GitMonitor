b699822dfd74bdf836535d85cc5137c18978243d
Michael Fourie <michaelfourie@gmail.com>
2016-04-04 21:11:36 +0100

    Use javascript for page refreshing. Maintain selection on page refresh

--- src/GitMonitor/wwwroot/js/site.js
+++ src/GitMonitor/wwwroot/js/site.js
@@ -1,1 +1,18 @@
-﻿// Write your Javascript code.
+﻿$(document).ready(function () {
+    if (location.hash) {
+        $('a[href=' + location.hash + ']').tab('show');
+    }
+    $(document.body).on("click", "a[data-toggle]", function (event) {
+        location.hash = this.getAttribute("href");
+    });
+});
+$(window).on('popstate', function () {
+    var anchor = location.hash || $("a[data-toggle=tab]").first().attr("href");
+    $('a[href=' + anchor + ']').tab('show');
+});
+
+var timeout = setTimeout("location.reload(true);", 60000);
+function resetTimeout() {
+    clearTimeout(timeout);
+    timeout = setTimeout("location.reload(true);", 60000);
+}
